f47a7e3751a81127a110e8c50bc9e0a3cd01eb26
Özgür Kibar <ozgurkibar78@gmail.com>
2018-02-28 14:11:43 +0100

    first template for Garage.java

--- src/models/Garage.java
+++ src/models/Garage.java
@@ -1,5 +1,35 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Garage {
 
+	List<Vehicle> vehicles;
+	
+	public void listAllParkedVehicles(){
+		 for(Vehicle v: vehicles) {
+			 System.out.println(v.toString());
+		 }
+	}
+	
+	public void listAllCurrentlyParkedTypes() {
+		for(Vehicle v : vehicles) {
+			//try to cast the vehicle to all types
+		}
+	}
+	
+	public void parkVehicle(Vehicle v) {
+		if(!.v.parked()) {
+			v.parkVehicle();
+		}
+	}
+	
+	public void setMaxCapacity(int newMax) {
+		maxCapacity = newMax;
+	}
+	
+	public Vehicle findVehicle() {
+		return null;
+	}
 }
