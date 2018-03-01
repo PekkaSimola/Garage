dfd7ded677663d60b3e005120327d4543bf771a7
Özgür Kibar <ozgurkibar78@gmail.com>
2018-03-01 09:58:47 +0100

    ändrat i Garage.java

--- src/models/Garage.java
+++ src/models/Garage.java
@@ -2,28 +2,41 @@ package models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Garage {
 
 	private int maxCapacity = 0;
 
-	List<Vehicle> vehicles;
+	Map<Integer, Vehicle> vehicles;
 
+	public Garage() {
+		
+	}
+	
 	public void listAllParkedVehicles() {
-		for (Vehicle v : vehicles) {
-			System.out.println(v.toString());
+		for (Vehicle vehicle : vehicles) {
+			System.out.println(vehicle.toString());
 		}
 	}
 
 	public void listAllCurrentlyParkedTypes() {
-		for (Vehicle v : vehicles) {
-			// try to cast the vehicle to all types
+		int cars = 0;
+		int boats = 0;
+		int aeroplanes = 0;
+		int buses = 0;
+		int motorcykles = 0;
+		for (Map.Entry<Integer, Vehicle> vehicle : vehicles.entrySet()) {
+			if((vehicle.getValue().instanceOf(Car)) {
+				cars++;
+			}
 		}
 	}
 
-	public void parkVehicle(Vehicle v) {
-		if(! vehicles.contains(v) ) {
-			vehicles.add(v);
+	public void parkVehicle(Vehicle vehicle) {
+		if(! vehicles.containsValue(vehicle) ) {
+			vehicles.put(nextKey++, vehicle);
 		}
 		else {
 			System.out.println("The vehicle is already parked in the garage.");
@@ -37,5 +50,5 @@ public class Garage {
 	public Vehicle findVehicle() {
 		return null;
 	}
-
+	
 }
