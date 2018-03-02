d76678eae97523a4819896f0e3c85d825902bb63
PekkaSimola <pekka1simola@gmail.com>
2018-03-02 09:11:48 +0100

    *Synch of garage.java and main.java

--- src/models/Garage.java
+++ src/models/Garage.java
@@ -149,11 +149,11 @@ public class Garage {
 		}	
 	}
 	
-	public void load() throws FileNotFoundException{
-		ArrayList<Vehicle> test = new ArrayList();
-		FileInputStream in = new FileInputStream("garage.lex");
-		Scanner sc = new Scanner(in);
-		test.addAll(sc.next());
-	}
-	
+//	public void load() throws FileNotFoundException{
+//		ArrayList<Vehicle> test = new ArrayList();
+//		FileInputStream in = new FileInputStream("garage.lex");
+//		Scanner sc = new Scanner(in);
+//		test.addAll(sc.next());
+//	}
+//	
 }
